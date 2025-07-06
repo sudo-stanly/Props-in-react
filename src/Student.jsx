5728bded8caf413836a847fcf599c6dd9154ae0f
stanly-sudo <alys.git24@gmail.com>
2025-07-06 09:51:09 +0800

    styled card

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,8 +1,11 @@
+import profilePicture from './assets/pfp.jpg'
+
 function Student(props)
 {
     return(
         <>
-            <div>
+            <div className="card">
+                <img src={profilePicture} alt="profile picture" className="card-image" />
                 <p>Name:{props.name}</p>
                 <p>Age:{props.age}</p>
                 <p>Student:{props.isStudent ? "Yes" : "No" }</p>
