37b4183de9d624df98dd70e2878da84f0506d9ac
stanly-sudo <alys.git24@gmail.com>
2025-07-06 10:05:01 +0800

    prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     <>
       <Student name="Tony Stark" age={30} isStudent={true}/> {/* changing pops of boolean to false will change the props property, usng ternary operator " ? true : false " */}
       <Student name="John Doe" age={65} isStudent={false}/>
+      <Student name="Jane Doe" age={42} isStudent={false}/>
+      <Student name="Bob Marley" age={22} isStudent={true}/>
     </>
   )
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,5 +1,5 @@
+import PropTypes from 'prop-types'
 import profilePicture from './assets/pfp.jpg'
-
 function Student(props)
 {
     return(
@@ -13,4 +13,13 @@ function Student(props)
         </>
     );
 }
+
+// proptypes : to check type of prop (string, int, etc...), errors will show in console incase of  failed prop type.
+
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+}
+
 export default Student;
